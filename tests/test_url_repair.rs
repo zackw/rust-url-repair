1923ff01b2a821aa044e0a652bae17bc926d4287
Zack Weinberg <zackw@panix.com>
2019-05-09 08:52:21 -0400

    Add a bunch more test cases, mostly hostname-related.

--- tests/test_url_repair.rs
+++ tests/test_url_repair.rs
@@ -9,25 +9,37 @@
 const SUCCESSFUL_CASES: &'static str = r#"
 # Equivalent forms of the scheme and hostname.
 http://example.com/            http://example.com/
-http://example.com/            http://example.com
 http://example.com/            HTTP://EXAMPLE.COM/
-http://example.com/            HTTP://EXAMPLE.COM
-http://example.com/            HTTP:EXAMPLE.COM
-http://example.com/            http:\\example.com\
+http://example.com/            http://ex%61mple%2Ecom/
 
-# Redundant port numbers should be stripped.
+# An incorrect number of slashes after the scheme should be corrected.
+http://example.com/            http:example.com
+http://example.com/            http:/example.com
+http://example.com/            http:///example.com
+
+# Redundant port numbers for "special" schemes should be stripped.
+ftp://example.com/             ftp://example.com:21/
+gopher://example.com/          gopher://example.com:70/
 http://example.com/            http://example.com:80/
-http://example.com:443/        http://example.com:443/
 https://example.com/           https://example.com:443/
-https://example.com:80/        https://example.com:80/
+ws://example.com/              ws://example.com:80/
+wss://example.com/             wss://example.com:443/
+
+# Non-redundant port numbers should be preserved.
+ftp://example.com:1108/        ftp://example.com:1108/
+gopher://example.com:1108/     gopher://example.com:1108/
+http://example.com:1108/       http://example.com:1108/
+https://example.com:1108/      https://example.com:1108/
+ws://example.com:1108/         ws://example.com:1108/
+wss://example.com:1108/        wss://example.com:1108/
 
 # Empty username and password should be stripped.
 # We don't do any other tests on these because they're so very deprecated.
 http://example.com/            http://@example.com/
-#http://example.com/           http://%@example.com/
-#http://user@example.com/      http://user%@example.com/
-http://%pass@example.com/      http://%pass@example.com/
-http://user%pass@example.com/  http://user%pass@example.com/
+http://example.com/            http://:@example.com/
+http://user@example.com/       http://user:@example.com/
+http://:pass@example.com/      http://:pass@example.com/
+http://user:pass@example.com/  http://user:pass@example.com/
 
 # Leading and trailing dots on the hostname are to be stripped.
 # Doubled dots in the hostname are to be collapsed.
@@ -36,7 +48,7 @@ http://user%pass@example.com/  http://user%pass@example.com/
 #http://example.com/           http://.example.com./
 #http://example.com/           http://example..com/
 
-# Unicode is to be converted to IDNA.
+# Unicode in the hostname is to be converted to IDNA.
 http://xn--hxajbheg2az3al.example/    http://παράδειγμα.example/
 
 # RFC-nonconformant hostnames are to be tolerated.
@@ -50,18 +62,24 @@ http://under_score.example/                  http://under_score.example/
 #http://mail.163.com.xn----9mcjf9b4dbm09f.com@domain.invalid/ http://mail.163.com.xn----9mcjf9b4dbm09f.com/
 #http://mail.163.com.xn----9mcjf9b4dbm09f.com@domain.invalid/ http://mail.163.com.روغن-کنجد.com/
 
-# Canonicalization of IPv4 address literals
+# IPv4 address literals
 http://127.0.0.1/   http://127.0.0.1/
 
-
-
-
-# leading zeroes are OK but should not cause the number to be
+# leading zeroes should be stripped and should NOT cause the number to be
 # interpreted as octal -- needs implementing manually, rust-url does
 # the Wrong Thing (on purpose, apparently, blech)
 #http://127.0.0.1/   http://127.000.000.001/
 #http://127.0.0.8/   http://127.000.000.008/
 #http://127.0.0.10/  http://127.000.000.010/
+
+# IPv6 address literals
+http://[::1]/    http://[::1]/
+http://[::1]/    http://[0:0:0:0:0:0:0:1]/
+http://[::1]/    http://[0000:0000:0000:0000:0000:0000:0000:0001]/
+
+http://[2001:db8::ff00:42:8329]/    http://[2001:db8::ff00:42:8329]/
+http://[2001:db8::ff00:42:8329]/    http://[2001:db8:0:0:0:ff00:42:8329]/
+http://[2001:db8::ff00:42:8329]/    http://[2001:0db8:0000:0000:0000:ff00:0042:8329]/
 
 # Scheme-relative URLs and bare hostnames should be filled in with the
 # default scheme, which is 'http'.
@@ -73,9 +91,50 @@ http://127.0.0.1/   http://127.0.0.1/
 http://example.com/ABOUT?x=Y#zED http://example.com/ABOUT?x=Y#zED
 http://example.com/ABOUT?x=Y#zED HTTP://EXAMPLE.com/ABOUT?x=Y#zED
 
+# An incorrect number of slashes after the hostname should be corrected.
+http://example.com/            http://example.com
+#http://example.com/           http://example.com//
+
+# Dot, dotdot, and slash normalization within the path.  The URL
+# standard doesn't require slash normalization, for no apparent reason.
+#http://example.com/about      http://example.com//about
+
+http://example.com/a/b/d       http://example.com/a/./b/c/../d
+http://example.com/a/b/d       http://example.com/./a/././b/./c/.././d
+http://example.com/a/b/d       http://example.com/a/b/e/../c/f/../../d
+#http://example.com/a/b/d      http://example.com//a//b//d
+
+# Trailing slashes cannot be dropped, but can be normalized.
+http://example.com/a/b/d/      http://example.com/a/b/d/
+#http://example.com/a/b/d/     http://example.com/a/b/d//
+http://example.com/a/b/d/      http://example.com/a/b/d/./
+
+# Backslashes in any component up to and including the path are to be
+# normalized to slashes.  Mixtures are allowed.
+http://example.com/                 http:\\example.com\
+http://example.com/about/backslash  http:\\example.com\about\backslash
+http://example.com/a/b/d            http:/\example.com/.\a/.\./b\.\c/../.\d
+http://example.com/a/b/d            http://example.com/a/b/e\..\c/f/..\..\d
+#http://example.com/a/b/d/          http://example.com\\a//b\/d/\
+
+# Backslashes are not to be normalized to slashes within query and fragment.
+http://example.com/?a=b\c           http://example.com/?a=b\c
+http://example.com/?a\b=c           http://example.com/?a\b=c
+http://example.com/#a=b\c           http://example.com/#a=b\c
+http://example.com/#a\b=c           http://example.com/#a\b=c
+
+# TODO write test cases for:
+# as many layers of %-quotation as possible are to be stripped
+# necessary %-quotation should be preserved in all components
+# characters that _don't_ need to be %-quoted should be unquoted
+# characters outside printable ASCII should be %-quoted in path,
+#  query, and fragment
+
 "#;
 
-#[allow(dead_code)] // unsuccessful tests not yet implemented
+// Same as above, but these are expected to parse unsuccessfully.
+// The left-hand side is the expected Debug serialization of the
+// url::ParseError that is returned.
 const UNSUCCESSFUL_CASES: &'static str = r#"
 # IPv4 address literals
 # individual components must be in the range 0..255
@@ -85,6 +144,8 @@ InvalidIpv4Address    http://1.1.256.1/
 InvalidIpv4Address    http://1.1.1.256/
 
 # these are network addresses, not host addresses
+# ??? unclear whether we should try to detect them
+# (where does one draw the line between usable and non-usable?)
 #InvalidIpv4Address    http://0.0.0.0/
 #InvalidIpv4Address    http://1.0.0.0/
 #InvalidIpv4Address    http://1.2.0.0/
@@ -103,42 +164,71 @@ InvalidIpv4Address    http://1.1.1.256/
 #InvalidIpv4Address   http://0x7f000001/
 #InvalidIpv4Address   http://017700000001/
 
+# IPv6 address literals
+# inappropriate characters within the square brackets
+InvalidIpv6Address    http://[xyz::1]/
+InvalidIpv6Address    http://[%4a::1]/
+
+# missing one or other square bracket
+InvalidIpv6Address    http://[::1/
+EmptyHost             http://::1]/
+
+# cannot use :: twice (it's ambiguous)
+InvalidIpv6Address    http://[2001:db8::ff00::42:8329]/
 "#;
 
 test_suite! {
     name test_url_repair;
 
     use url_repair::repair_url;
+    use std::iter::Iterator;
+
+    fn parse_test_list (s: &str) -> impl Iterator<Item=(&str, &str)> {
+        s.lines().filter_map(|line| {
+            if line.is_empty() || line.starts_with("#") {
+                None
+            } else {
+                let mut fields = line.split_whitespace();
+                let result = fields.next().expect("expected result missing");
+                let input = fields.next().expect("input text missing");
+                assert_eq!(fields.next(), None, "too many fields");
+                Some((result, input))
+            }
+        })
+    }
 
     fixture successful_case(parsed: &'static str, input: &'static str) -> () {
         params {
-            use crate::SUCCESSFUL_CASES;
-            SUCCESSFUL_CASES.lines()
-                .filter_map(|line| {
-                    if line.is_empty() || line.starts_with("#") {
-                        None
-                    } else {
-                        let mut fields = line.split_ascii_whitespace();
-                        let parsed = fields.next().expect("parsed url missing");
-                        let input = fields.next().expect("input text missing");
-                        assert_eq!(fields.next(), None, "too many fields");
-                        Some((parsed, input))
-                    }
-                })
+            parse_test_list(crate::SUCCESSFUL_CASES)
         }
         setup(&mut self) {}
     }
     test successful_repairs(successful_case) {
-        let input = successful_case.params.input;
-        let parsed = successful_case.params.parsed;
+        let input = *successful_case.params.input;
+        let parsed = *successful_case.params.parsed;
 
         match repair_url(input) {
-            Ok(url) => {
-                assert_eq!(url.as_str(), *parsed);
-            }
-            Err(err) => {
-                assert!(false, "{}: unsuccessful repair: {}", input, err);
-            }
+            Ok(url)  => assert_eq!(url.as_str(), parsed),
+            Err(err) => assert!(false, "{}: unsuccessful repair: {}",
+                                input, err)
+        }
+    }
+
+    fixture unsuccessful_case(xerr: &'static str, input: &'static str) -> () {
+        params {
+            parse_test_list(crate::UNSUCCESSFUL_CASES)
+        }
+        setup(&mut self) {}
+    }
+    test unsuccessful_repairs(unsuccessful_case) {
+        let input = *unsuccessful_case.params.input;
+        let xerr = *unsuccessful_case.params.xerr;
+
+        match repair_url(input) {
+            Err(err) => assert_eq!(format!("{:?}", err), xerr),
+            Ok(url)  => assert!(false,
+                                "{}: should not parse (got {}, expected {})",
+                                input, url, xerr)
         }
     }
 }
